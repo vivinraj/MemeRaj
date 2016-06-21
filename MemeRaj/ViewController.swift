a60e1a967a478b222a2e3964876af8e3bd3948d6
Raj <vivinraj@gmail.com>
2016-06-21 08:48:21 +0530

    Initial Commit

---
+++ MemeRaj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeRaj
+//
+//  Created by Vivin Raj on 21/06/16.
+//  Copyright © 2016 Vivin Raj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
