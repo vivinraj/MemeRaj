7318fb9a434522927f75fa206c01d1e344177083
Raj <vivinraj@gmail.com>
2016-06-23 21:59:00 +0530

    Keyboard notification implemented but not working

--- MemeRaj/ViewController.swift
+++ MemeRaj/ViewController.swift
@@ -11,8 +11,19 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
-    
     @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var topLabel: UITextField!
+    @IBOutlet weak var bottomLabel: UITextField!
+    
+    
+    
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName : UIColor.blackColor(),
+        NSForegroundColorAttributeName : UIColor.whiteColor(),
+        NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!
+       // NSStrokeWidthAttributeName : NSNumber(float: -4.0)
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +32,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func viewWillAppear() {
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        topLabel.defaultTextAttributes = memeTextAttributes
+        bottomLabel.defaultTextAttributes = memeTextAttributes
+        subscribeToKeyboardNotifications()
+        }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
+    
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        print("subscribeToKeyNotifications")
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    
+    func keyboardWillShow(notification: NSNotification) {
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        print("get height")
+        return keyboardSize.CGRectValue().height
+        
+    }
+    
 
  
     @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
