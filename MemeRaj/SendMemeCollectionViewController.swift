734416c842fb0128cab1372333e3a9a5a46e550b
Vivin Raj <vivinraj@gmail.com>
2016-07-12 00:19:49 +0530

    Add meme buttons added to collection and table view and working

---
+++ MemeRaj/SendMemeCollectionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SendMemeViewController.swift
+//  MemeRaj
+//
+//  Created by Vivin Raj on 11/07/16.
+//  Copyright © 2016 Vivin Raj. All rights reserved.
+//
+
+
+import Foundation
+
+import UIKit
+
+class SendMemeCollectionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem (
+            title: "Add Meme",
+            style: UIBarButtonItemStyle.Plain,
+            target: self,
+            action: "addMeme")
+    }
+    
+    func addMeme() {
+        print("pop view controller")
+        if let navigationController = self.navigationController {
+            var Controller: ViewController
+            Controller = self.storyboard?.instantiateViewControllerWithIdentifier("MemeViewController") as! ViewController
+            print("add meme called")
+          //  navigationController.presentViewController(self.ViewController, animated: true, completion: nil)
+            navigationController.presentViewController(Controller, animated: true, completion: nil)
+            
+        }
+    }
+
+    
+}
+
